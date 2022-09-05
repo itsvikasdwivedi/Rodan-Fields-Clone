d57a9bff5b42bb08b5a9ef0316e5c865a21b91cb
Gulshan7777 <sharmagulshan252@gmail.com>
2022-09-05 15:43:36 +0530

    footer is done

---
+++ footer.js
@@ -0,0 +1,41 @@
+export const footer=()=>{
+    document.getElementById("footer").innerHTML=`
+    <div id="footerHead">
+            <div>
+                <ul>
+                    <li class="firstHead">ABOUT R+F</li>
+                    <li>PRESS ROOM</li>
+                    <li>COMPANY INFO</li>
+                    <li>EXECUTIVE TEAM</li>
+                    <li>CAREERS</li>
+                    <li>R+F RECYCLING PROGRAM</li>
+                    <li>BLOG</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li class="firstHead">HELP</li>
+                    <li>PRODUCT & HELP CENTER</li>
+                    <li>SHIPPING</li>
+                    <li>SATISFACTION GUARANTEE</li>
+                    <li>PROMOTIONAL TERMS & CONDITIONS</li>
+                    <li>R+F FAQS</li>
+                    <li>KLARNA</li>
+                    <li>SITE MAP</li>
+                </ul>
+            </div>
+            <div>
+                <div class="footercontent">Let’s Get Social</div>
+                <div class="socialIcons">
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-instagram"></i>
+                    <i class="fa-brands fa-youtube"></i>
+                    <i class="fa-brands fa-pinterest-p"></i>
+                </div>
+            </div>
+        </div>
+        <div id="footerFoot">
+            © 2022 Rodan & Fields &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Privacy Policy&nbsp;&nbsp; | &nbsp;&nbsp;Terms & Conditions&nbsp;&nbsp; | &nbsp;&nbsp;Accessibility&nbsp;&nbsp; | &nbsp;&nbsp;California Supply Chains Act&nbsp;&nbsp; | &nbsp;&nbsp;DSA Code Of Ethics&nbsp;&nbsp; | &nbsp;&nbsp;Disclaimer&nbsp;&nbsp; | &nbsp;&nbsp;Preference Center&nbsp;&nbsp; </div>
+    `
+}
