79aaf429f01d3f4b36261ce764eef0050549b4ff
Gulshan7777 <sharmagulshan252@gmail.com>
2022-09-07 20:09:08 +0530

    cart page

---
+++ cart.js
@@ -0,0 +1,152 @@
+import {footer} from "../footer.js";
+footer();
+
+var product=[{
+        "Id": 1,
+        "title": "R+F Instant Targeted Firming Gel™",
+        "image": "https://www.rodanandfields.com/en-us/medias/ENG010-What-sIncluded-570x570.png?context=bWFzdGVyfGltYWdlc3wxODU2NXxpbWFnZS9wbmd8aW1hZ2VzL2g5YS9oNjUvMTQ5NjkwODY1NDE4NTQucG5nfGNlNDBkMDdkYzlkMDFmZDQ1MDg4ZDFmMDA0MmUzYzc2NjgwNmQ3ZDgxNWExYmY3YWRmYzcwYzYyOWY3NTkyZmQ",
+        "status": "New Arrival",
+        "info": "Multi-targeted skincare treatment that instantly and visibly firms, tightens + smooths.",
+        "price": 105,
+        "rating": "",
+        "rating_votes": "",
+        "perk_price": 95,
+        "category": "Face Serums",
+        "application_area": "Face",
+        "skin_concern": "Fine Lines & Wrinkles",
+        "product_line": "Redefine",
+        "img_1": "https://www.rodanandfields.com/en-us/medias/ENFG010-900X900-MOBILE.jpg?context=bWFzdGVyfGltYWdlc3wyMzM5OHxpbWFnZS9qcGVnfGltYWdlcy9oZjAvaDMwLzE0OTY5MDg2NjQwMTU4LmpwZ3w2MDM5ODk3MDQwYzQ5ZTRjNzQ3OTBlN2E4ZmYwMGM3ZDlkYWVmNDBmMTc5ZmY3ZGE0ZWRlZTdjNmFiZTJjMTc1",
+        "img_2": "https://www.rodanandfields.com/en-us/medias/ENFG010-ITFG-Lifesyle-570x570.jpg?context=bWFzdGVyfHJvb3R8NTc2NTl8aW1hZ2UvanBlZ3xoMzAvaDBiLzE0OTY5MDg2OTM1MDcwLmpwZ3xhYjhkYjI2NTlkY2JkNjIyODVkM2VhNDU4NjVjNWVlNDc3NGUzYWM3MTE5ZTNjYTAyM2QxYWM3ZTFmMWExZGU5",
+        "img_3": "https://www.rodanandfields.com/en-us/medias/ENFG010-ITFG-Texture1-570x570.jpg?context=bWFzdGVyfHJvb3R8ODE2N3xpbWFnZS9qcGVnfGhkOS9oMzcvMTQ5NjkwODcxNjQ0NDYuanBnfGJlODFiNDMzNGJmODAwOTJkZWQwY2ExNWMzMjJiMzU0Mjc2ZjNhNjE4NjdjZDg4MmNkZTNjYzE3MjZjZmM1MDk",
+        "img_4": "https://www.rodanandfields.com/en-us/medias/ENFGR10-ITFG-Refill-GIF-570x570.gif?context=bWFzdGVyfHJvb3R8NDAyNTkwfGltYWdlL2dpZnxoMjEvaDA1LzE0OTY5MDg3NTU3NjYyLmdpZnw4NTNlNDQ4NWZiYzdhMjkwNzQ0MDUzNTA5OGI5Nzg3MjVlN2NlYjI1MzdhYTNhNGE5MjJjZTI1MTlkMjU4NDVj"
+},{
+    "Id": 2,
+    "title": "R+F Instant Targeted Firming Gel Refill™",
+    "image": "https://www.rodanandfields.com/en-us/medias/ENFGR10-720X600-DESKTOP.jpg?context=bWFzdGVyfGltYWdlc3wxMzY4MXxpbWFnZS9qcGVnfGltYWdlcy9oZjQvaGIwLzE0OTY5MDg3MDY2MTQyLmpwZ3wwODNjOTcwMTFkMjZjMWI1MTVkN2VhMTViNWI0ZTUyMzY0MDc0ZGI3YmY0YzlmNmNlN2FlZTI4YzY2M2FjODc2",
+    "status": "New Arrival",
+    "info": "A refill vial of our multi-targeted skincare treatment for minimal packaging waste.",
+    "price": 87,
+    "rating": "",
+    "rating_votes": "",
+    "perk_price": 78,
+    "category": "Face Serums",
+    "application_area": "Face",
+    "skin_concern": "Fine Lines & Wrinkles",
+    "product_line": "Redefine",
+    "img_1": "https://www.rodanandfields.com/en-us/medias/ENFGR10-900X900-MOBILE.jpg?context=bWFzdGVyfGltYWdlc3wyMTA4MXxpbWFnZS9qcGVnfGltYWdlcy9oNTMvaGJkLzE0OTY5MDg3MTk3MjE0LmpwZ3w0OTUzOGFjMjE0MWMxYzBhMGQ4ZTAwMDk3ZDhiYzI2YWEwN2ZmNTk1MzE0NDY4ZmM4YmFjZWQ2MjQ2NmZkZTAx",
+    "img_2": "https://www.rodanandfields.com/en-us/medias/ENFG010-ITFG-Lifesyle-570x570.jpg?context=bWFzdGVyfHJvb3R8NTc2NTl8aW1hZ2UvanBlZ3xoMzAvaDBiLzE0OTY5MDg2OTM1MDcwLmpwZ3xhYjhkYjI2NTlkY2JkNjIyODVkM2VhNDU4NjVjNWVlNDc3NGUzYWM3MTE5ZTNjYTAyM2QxYWM3ZTFmMWExZGU5",
+    "img_3": "https://www.rodanandfields.com/en-us/medias/ENFG010-ITFG-Texture1-570x570.jpg?context=bWFzdGVyfHJvb3R8ODE2N3xpbWFnZS9qcGVnfGhkOS9oMzcvMTQ5NjkwODcxNjQ0NDYuanBnfGJlODFiNDMzNGJmODAwOTJkZWQwY2ExNWMzMjJiMzU0Mjc2ZjNhNjE4NjdjZDg4MmNkZTNjYzE3MjZjZmM1MDk",
+    "img_4": "https://www.rodanandfields.com/en-us/medias/ENFGR10-ITFG-Refill-GIF-570x570.gif?context=bWFzdGVyfHJvb3R8NDAyNTkwfGltYWdlL2dpZnxoMjEvaDA1LzE0OTY5MDg3NTU3NjYyLmdpZnw4NTNlNDQ4NWZiYzdhMjkwNzQ0MDUzNTA5OGI5Nzg3MjVlN2NlYjI1MzdhYTNhNGE5MjJjZTI1MTlkMjU4NDVj"
+  },
+  {
+    "Id": 3,
+    "title": "R + F Lash Boost",
+    "image": "https://www.rodanandfields.com/en-us/medias/ENHLSH01-Regimen-Explainer-R-F-Lash-Boost-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3wxMDc0OHxpbWFnZS9wbmd8aW1hZ2VzL2hmMS9oYWMvMTQ1MzIwNDI1ODgxOTAucG5nfDQyNTQ0OGUzYmMwZTJkM2YzNzE1MjE2NzQ1M2YxMWQ5M2E3ZWQ2MjAwMDA3MzFmNjQyOWUwNDU3OWZmMTkzZDY",
+    "status": "Best Seller",
+    "info": "Applied nightly to promote the appearance of longer, stronger and darker-looking lashes.",
+    "price": 155,
+    "rating": "4.6",
+    "rating_votes": "(8276)",
+    "perk_price": 140,
+    "category": "Face Serums",
+    "application_area": "Eyelashes ",
+    "skin_concern": "Fine Lines & Wrinkles",
+    "product_line": "Redefine",
+    "img_1": "https://www.rodanandfields.com/en-us/medias/ENHLSH01-detail.jpg?context=bWFzdGVyfGltYWdlc3wzMjIzMHxpbWFnZS9qcGVnfGltYWdlcy9oOGEvaGRiLzg4MjE4NjAyMDQ1NzQuanBnfDI5N2MzYWVlOTg4NjBhYzJiYzk0ZTY2ODU0YTk1OGY1NTFmMWFjMjIxMTRjNzUwNmUyZTJlM2JmMzhiZDIzZDU",
+    "img_2": "https://www.rodanandfields.com/en-us/medias/ENHLSH01-Lash-Carousel-51x51.jpg?context=bWFzdGVyfGltYWdlc3wzMTQyfGltYWdlL2pwZWd8aW1hZ2VzL2hmNi9oM2UvMTUzOTU0MTQ5MzM1MzQuanBnfDRmMjZhNzczNjc0NjYzZjkyMDhhMDBiODgwNzMxYzBiYTcxMjg3MDY2YmFmODdlMTliYTk5NzBkZTcxN2UzMDQ",
+    "img_3": "https://www.rodanandfields.com/en-us/medias/ENHLSH01-PDP-lifestyle-1168x1168-MOBILE.jpg?context=bWFzdGVyfGltYWdlc3w3NTIxNnxpbWFnZS9qcGVnfGltYWdlcy9oYzUvaDMxLzg4MjE4Nzg5MTUxMDIuanBnfDI2MDVlY2NlZDcxNzIxN2MzM2JiZWEwZTFkYTdjYzljYWVhMGNkODlhOTk2Y2Y0NDdhOTdlZjkyZjdiZDhiMzQ"
+  }]
+localStorage.setItem("list",JSON.stringify(product));
+var pro=localStorage.getItem("list")||[];
+  const showdata=(pro)=>{
+      var proHere=document.getElementById("selectedProduct");
+      proHere.innerHTML="";
+      pro.map(el=>{
+        let cartCount=1;
+        var div = document.createElement("div");
+        div.setAttribute("class","g_card_cart")
+        
+        var img = document.createElement("img");
+        img.src=el.image;
+        
+        var div1 = document.createElement("div");
+        
+        var div2 = document.createElement("div");
+        
+        var p =document.createElement("p");
+        p.setAttribute("id","g_productName")
+        p.innerHTML=el.title;
+        
+        var div3 = document.createElement("div");
+        div3.setAttribute("id","g_btns");
+        
+        
+        var btn1 = document.createElement("button");
+        btn1.setAttribute("id","g_decbtn");
+        btn1.innerHTML=`<i class="fa-solid fa-minus"></i>`
+        
+        var inputcart = document.createElement("input");
+        inputcart.value=cartCount;
+        inputcart.type="number";
+        inputcart.setAttribute("id","productCount")
+        inputcart.setAttribute("class","inputcount")
+        inputcart.setAttribute("min","0")
+        inputcart.setAttribute("max","999")
+
+        
+        var btn2 = document.createElement("button");
+        btn2.setAttribute("id","g_incbtn");
+        btn2.innerHTML=`<i class="fa-solid fa-plus"></i>`
+        btn2.onclick=()=>{
+            inputcart.value=++cartCount;
+        }
+        
+        var div4 = document.createElement("div");
+        div4.setAttribute("class","lastDivcart");
+        var delbtn =document.createElement("button");
+        delbtn.innerHTML=`<i class="fa-solid fa-trash"></i>`
+        delbtn.onclick=()=>{
+
+            var delItem=pro.reduce(del=>{
+                return del.Id
+            })
+               
+            console.log(delItem);
+            // showdata(product)
+            // console.log(event.target.parentNode.parentNode.parentNode)
+        }
+
+        var p2 = document.createElement("p");
+        p2.innerHTML=`$${el.price}.00`;
+
+        div3.append(btn1,inputcart,btn2);
+        div4.append(delbtn,p2);
+        div2.append(p,div3);
+        div1.append(div2,div4);
+        div.append(img,div1);
+        proHere.append(div);
+    })
+    // document.getElementById("g_incbtn").addEventListener("click",()=>{
+    //     document.getElementById("g_countproduct").value=++cartCount;
+    //     // var da=parseInt(document.getElementById("g_countproduct").value,10);
+        
+    //     // console.log(da);
+        
+    // });
+}
+
+window.addEventListener("load",()=>showdata(product));
+
+
+
+
+
+
+const increaseIt=()=>{
+    console.log("skdj")
+    document.getElementById("productCount").value=5;
+    // document.getElementById("g_incbtn")
+    // console.log(inputcart.value)
+}
+
+// document.getElementById("g_incbtn").addEventListener("click",increaseIt)
