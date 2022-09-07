1cb11373261db69b4adc4613c2bd1e8da79292b7
Gulshan7777 <sharmagulshan252@gmail.com>
2022-09-08 00:38:53 +0530

    cart page is done except localstorage

--- cart page/cart.js
+++ cart page/cart.js
@@ -1,4 +1,4 @@
-import {footer} from "../footer.js";
+import {footer} from "../../footer.js";
 footer();
 
 var product=[{
@@ -58,47 +58,88 @@ var product=[{
   }]
 localStorage.setItem("list",JSON.stringify(product));
 var pro=localStorage.getItem("list")||[];
-  const showdata=(pro)=>{
-      var proHere=document.getElementById("selectedProduct");
-      proHere.innerHTML="";
-      pro.map(el=>{
+let totalCost=0;
+const showdata=(pro)=>{
+    var proHere=document.getElementById("selectedProduct");
+    proHere.innerHTML="";
+    if(pro.length==0){
+      document.getElementById("cardHead").innerHTML="";
+      var err=document.getElementById("cart_container");
+      err.innerHTML="";
+      err.innerHTML=`
+      <div id="errHead">
+        <H3>Your Bag Is Empty</H3>
+        <p>Shop now to add products to your bag.</p>
+        <button id="startShop"><a href="#">START SHOPPING</a></button>
+        </div>
+      `
+    }else{
+      pro.map((el,index)=>{
         let cartCount=1;
-        var div = document.createElement("div");
-        div.setAttribute("class","g_card_cart")
-        
-        var img = document.createElement("img");
-        img.src=el.image;
-        
-        var div1 = document.createElement("div");
-        
-        var div2 = document.createElement("div");
-        
-        var p =document.createElement("p");
-        p.setAttribute("id","g_productName")
-        p.innerHTML=el.title;
-        
-        var div3 = document.createElement("div");
-        div3.setAttribute("id","g_btns");
-        
-        
-        var btn1 = document.createElement("button");
-        btn1.setAttribute("id","g_decbtn");
-        btn1.innerHTML=`<i class="fa-solid fa-minus"></i>`
-        
-        var inputcart = document.createElement("input");
-        inputcart.value=cartCount;
-        inputcart.type="number";
-        inputcart.setAttribute("id","productCount")
-        inputcart.setAttribute("class","inputcount")
-        inputcart.setAttribute("min","0")
-        inputcart.setAttribute("max","999")
-
-        
-        var btn2 = document.createElement("button");
-        btn2.setAttribute("id","g_incbtn");
-        btn2.innerHTML=`<i class="fa-solid fa-plus"></i>`
-        btn2.onclick=()=>{
-            inputcart.value=++cartCount;
+      var div = document.createElement("div");
+      div.setAttribute("class","g_card_cart")
+      
+      var img = document.createElement("img");
+      img.src=el.image;
+      
+      var div1 = document.createElement("div");
+      
+      var div2 = document.createElement("div");
+      
+      var p =document.createElement("p");
+      p.setAttribute("id","g_productName")
+      p.innerHTML=el.title;
+      
+      var div3 = document.createElement("div");
+      div3.setAttribute("id","g_btns");
+      
+      
+      var btn1 = document.createElement("button");
+      btn1.setAttribute("id","g_decbtn");
+      btn1.innerHTML=`<i class="fa-solid fa-minus"></i>`
+      btn1.onclick=()=>{
+        if(inputcart.value<=1){
+          deleteIt(index);
+        }else{
+          inputcart.value=--cartCount;
+          var price=inputcart.value*el.price;
+          console.log(price)
+          p2.innerHTML=`$${price}.00`
+          totalCost -=+el.price;
+          console.log(totalCost);
+          document.getElementById("CartPrice").innerHTML=`
+          $${totalCost}`
+          document.getElementById("cartSaving").innerHTML=`
+          $${Math.floor(totalCost*0.1)}`
+          document.getElementById("CartTotal").innerHTML=`
+          $${totalCost-Math.floor(totalCost*0.1)}`
+        }
+    }
+    
+    var inputcart = document.createElement("input");
+    inputcart.value=cartCount;
+    inputcart.type="number";
+    inputcart.setAttribute("id","productCount")
+    inputcart.setAttribute("class","inputcount")
+    inputcart.setAttribute("min","0")
+    inputcart.setAttribute("max","999")
+    
+    
+    var btn2 = document.createElement("button");
+    btn2.setAttribute("id","g_incbtn");
+    btn2.innerHTML=`<i class="fa-solid fa-plus"></i>`
+    btn2.onclick=()=>{
+      inputcart.value=++cartCount;
+      var price=inputcart.value*el.price
+      console.log(price)
+      p2.innerHTML=`$${price}.00`
+      totalCost +=+el.price;
+      document.getElementById("CartPrice").innerHTML=`
+      $${totalCost}`
+          document.getElementById("cartSaving").innerHTML=`
+          $${Math.floor(totalCost*0.1)}`
+          document.getElementById("CartTotal").innerHTML=`
+          $${totalCost-Math.floor(totalCost*0.1)}`
         }
         
         var div4 = document.createElement("div");
@@ -106,16 +147,8 @@ var pro=localStorage.getItem("list")||[];
         var delbtn =document.createElement("button");
         delbtn.innerHTML=`<i class="fa-solid fa-trash"></i>`
         delbtn.onclick=()=>{
-
-            var delItem=pro.reduce(del=>{
-                return del.Id
-            })
-               
-            console.log(delItem);
-            // showdata(product)
-            // console.log(event.target.parentNode.parentNode.parentNode)
+          deleteIt(index);
         }
-
         var p2 = document.createElement("p");
         p2.innerHTML=`$${el.price}.00`;
 
@@ -125,28 +158,33 @@ var pro=localStorage.getItem("list")||[];
         div1.append(div2,div4);
         div.append(img,div1);
         proHere.append(div);
+
+        totalCost +=+p2.innerHTML.split("$")[1];
+        document.getElementById("CartPrice").innerHTML=`
+        $${totalCost}`
+        document.getElementById("cartSaving").innerHTML=`
+        $${Math.floor(totalCost*0.1)}`
+        document.getElementById("CartTotal").innerHTML=`
+        $${totalCost-Math.floor(totalCost*0.1)}`
     })
-    // document.getElementById("g_incbtn").addEventListener("click",()=>{
-    //     document.getElementById("g_countproduct").value=++cartCount;
-    //     // var da=parseInt(document.getElementById("g_countproduct").value,10);
-        
-    //     // console.log(da);
-        
-    // });
+  }
+}
+  function deleteIt(ind){
+  pro.splice(ind,1);
+  localStorage.setItem("item",JSON.stringify(pro));
+  showdata(pro);
 }
 
 window.addEventListener("load",()=>showdata(product));
 
-
-
-
-
-
-const increaseIt=()=>{
-    console.log("skdj")
-    document.getElementById("productCount").value=5;
-    // document.getElementById("g_incbtn")
-    // console.log(inputcart.value)
-}
-
-// document.getElementById("g_incbtn").addEventListener("click",increaseIt)
+document.getElementById("couponbtn").addEventListener("click",()=>{
+  var val = document.getElementById("coupon").value;
+  if(val=='123456789'){
+    var cpn=document.getElementById("cpn");
+    cpn.classList.add("here");
+    document.getElementById("cDis").innerHTML=`$${10}`
+    var fTotal=+document.getElementById("CartTotal").innerText.split("$")[1]-10;
+    document.getElementById("CartTotal").innerHTML=`
+    $${fTotal}`
+  }
+})
